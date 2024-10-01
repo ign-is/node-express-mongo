1134aa3df71d7af8748803695489ad90ea163d23
ign-is <igna.dev.contact@gmail.com>
2024-10-01 14:10:21 -0300

    Add createServer function to server.js

--- 05TUT/server.js
+++ 05TUT/server.js
@@ -1,16 +1,56 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+const fsPromises = require('fs').promises;
+
 const logEvents = require('./logEvents');
-
 const EventEmitter = require('events');
-
-class MyEmitter extends EventEmitter {};
+class Emitter extends EventEmitter {};
 
 // intialize object
-const myEmitter = new MyEmitter();
+const myEmitter = new Emitter();
 
-// add listener for the log event
-myEmitter.on('log', (msg) => logEvents(msg));
+const PORT = process.env.PORT || 3500;
 
-setTimeout(() => {
-    //Emit event
-    myEmitter.emit('log', 'Log event emitted!');
-}, 2000)
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method);
+
+    const extension = path.extname(req.url);
+
+    let contentType;
+
+    switch (extension) {
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.jpg':
+            contentType = 'image/jpeg';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;
+        case '.txt':
+            contentType = 'text/plain';
+            break;
+        default:
+            contentType = 'text/html';
+    }
+
+    let filePath =
+    contentType === 'text/html' && req.url === '/'
+        ? path.join(__dirname, 'views', 'index.html')
+        : contentType === 'text/html' && req.url.slice(-1) === '/'
+            ? path.join(__dirname, 'views', req.url, 'index.html')
+            : contentType === 'text/html'
+                ? path.join(__dirname, 'views', req.url)
+                : path.join(__dirname, req.url);
+
+})
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
