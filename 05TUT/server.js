9d172913e8416800d9528dfdc319f6e2bbf993a8
ign-is <igna.dev.contact@gmail.com>
2024-10-02 13:42:29 -0300

    Add server update, styles and img

--- 05TUT/server.js
+++ 05TUT/server.js
@@ -12,6 +12,29 @@ const myEmitter = new Emitter();
 
 const PORT = process.env.PORT || 3500;
 
+const serveFile = async (filePath, contentType, response) => {
+    try {
+        const rawData = await fsPromises.readFile(
+            filePath,
+            !contentType.includes('image') ? 'utf8' : ''
+        );
+        const data = contentType === 'application/json'
+            ? JSON.parse(rawData) : rawData;
+        response.writeHead(
+            filePath.includes('404.html') ? 404 : 200,
+            { 'Content-Type': contentType }
+        );
+        response.end(
+            contentType === 'application/json' ? JSON.stringify(data) : data
+        );
+    } catch (err) {
+        console.log(err);
+        myEmitter.emit('log', `${err.name}: ${err.message}`, 'errLog.txt');
+        response.statusCode = 500;
+        response.end();
+    }
+}
+
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method);
 
@@ -51,6 +74,29 @@ const server = http.createServer((req, res) => {
                 ? path.join(__dirname, 'views', req.url)
                 : path.join(__dirname, req.url);
 
-})
+
+    // makes .html extension not required in the browser
+    if (!extension && req.url.slice(-1) !== '/') filePath += '.html';
+
+    const fileExists = fs.existsSync(filePath);
+
+    if (fileExists) {
+        serveFile(filePath, contentType, res);
+    } else {
+        switch(path.parse(filePath).base) {
+            case 'old-page.html':
+                res.writeHead(301, { 'Location': '/new-page.html' });
+                res.end();
+                break;
+            case 'www-page.html':
+                res.writeHead(301, { 'Location': '/' });
+                res.end();
+                break;
+            default: 
+                //serve a 404 response
+                serveFile(path.join(__dirname, 'view', '404.html'), 'text/html', res);
+        }
+    }
+});
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
