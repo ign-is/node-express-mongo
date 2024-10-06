9f5ed33a4121dd63c45f383e6360166e78a3cf60
ign-is <igna.dev.contact@gmail.com>
2024-10-06 16:26:06 -0300

    Add built-in, custom and third party middleware

--- 07TUT/server.js
+++ 07TUT/server.js
@@ -1,7 +1,37 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
+const { logger } = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+const whitelist = ['https://www.yoursite.com', 'http://127.0.0.1:5500', 'http://localhost:3500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whitelist.indexOf(origin) !== 1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(logger);
+
+// Cross Origin Resource Sharing 
+app.use(cors(corsOptions));
+
+// built-in middleware to hanlde urlencoded data
+// in other words, from data:
+// 'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// build-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('^/$|/index(.html)?', (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
