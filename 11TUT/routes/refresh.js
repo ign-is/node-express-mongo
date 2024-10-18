2969773d26611c2ebae722addc74ce99ce2ba2d5
ign-is <igna.dev.contact@gmail.com>
2024-10-18 16:44:24 -0300

    Add fixed refresh

--- 11TUT/routes/refresh.js
+++ 11TUT/routes/refresh.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const refreshTokenController = require('../controllers/refreshTokenController');
 
-router.get('/', refreshTokenController.handleLogin);
+router.get('/', refreshTokenController.handleRefreshToken);
 
 module.exports = router;
